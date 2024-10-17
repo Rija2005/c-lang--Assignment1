40bf032614ab37ff49ab7b7ec161fc194a2881c6
Rija2005 <164767074+Rija2005@users.noreply.github.com>
2024-10-17 10:07:55 -0700

    my work

---
+++ name.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Name:Syeda Reja Fatima\n Father Name:Syed Mohiuddin\n CNIC:00041233\n Qualification:BSAI\n Semester:1semester\n CGPA:4.0\n";
+    cout<<"Question3:\n1. <iostream>\nFunctions\n:std::cin: Input from keyboard\n.2. <string>\nFunctions\n:std::string: \nHandle strings.size(): Get string length.\n3. <vector>\nFunctions\n:std::vector<T>\n: Dynamic array.push_back(): Add element.\n4. <cmath>\nFunctions\n:std::sqrt(): Square root.\n";
+    cout<<"Question 4\n Preprocessing: Handles directives, produces expanded source.\nCompilation: Translates to assembly language.\nAssembly: Converts assembly to machine code.\nLinking: Combines object files into an executable.\n";
+   cout<<"Question no5\n COMPUTER LANGUAGES:\n HTML,CSS,Javascript,Java,C++,Python,Ruby,PHP,Swift\n";
+    return 0;
+}
